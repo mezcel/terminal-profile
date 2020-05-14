c6e1a451ff341529f3b78b8a28bfa2b88aa5a6a3
mezcel <mezcel@hotmail.com>
2020-05-13 21:34:18 -0400

    rm takskill

--- install.bat
+++ install.bat
@@ -10,6 +10,7 @@
     :: Local Path Vars
     set thisScriptName=%0
     set thisScriptParentDir=%~dp0
+    set stepCounter=0
 
 :greetings
     ECHO :::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -28,31 +29,31 @@
     set dt=%dt: =0%
 
 :::: close Windows Terminal
-ECHO :: Step 1. Close any open Windows Terminal sessions.
-:stopWT
+::ECHO :: Step 1. Close any open Windows Terminal sessions.
+:::stopWT
+    ::::taskkill /IM "WindowsTerminal.exe"
+    ::tasklist /fi "imagename eq WindowsTerminal.exe" |find ":" > nul
+    ::if errorlevel 1 (
+    ::    ECHO.
+    ::    ECHO Windows Terminal is running.
+    ::    ECHO    Launch this script again from a stand alone Command Prompt.
+    ::    ECHO    It is generally a good practice not make changes to an application while it is running.
+    ::    ECHO    This script is going to terminate all instances of Windows Terminal. ( just for redundant safety )
+    ::    ECHO    Finish whatever is going on in any other tabs or WT sessions before continuing.
+    ::    ECHO    Execute intall.bat again in any other Command Prompt terminal.
+    ::    ECHO.
+    ::
+    ::    set /p pausePrompt="Press ENTER to Launch an new instance of Command Prompt and close Windows Terminal."
+    ::    start /I cmd "title" "terminal-profile.git"
+    ::
+    ::    taskkill /f /im WindowsTerminal.exe
 
-    ::taskkill /IM "WindowsTerminal.exe"
-    tasklist /fi "imagename eq WindowsTerminal.exe" |find ":" > nul
-    if errorlevel 1 (
-        ECHO.
-        ECHO Windows Terminal is running.
-        ECHO    Launch this script again from a stand alone Command Prompt.
-        ECHO    It is generally a good practice not make changes to an application while it is running.
-        ECHO    This script is going to terminate all instances of Windows Terminal. ( just for redundant safety )
-        ECHO    Finish whatever is going on in any other tabs or WT sessions before continuing.
-        ECHO    Execute intall.bat again in any other Command Prompt terminal.
-        ECHO.
-
-        set /p pausePrompt="Press ENTER to Launch an new instance of Command Prompt and close Windows Terminal."
-        start cmd
-
-        taskkill /f /im "WindowsTerminal.exe" )
-
-    ECHO :: done.
-    ECHO.
+    ::ECHO :: done.
+    ::ECHO.
 
 :::: copy backgrounds and icons to RoamingState
-ECHO :: Step 2. Copy and backup the RoamingState directory.
+set stepCounter=%stepCounter%+1
+ECHO :: Step %stepCounter%. Copy and backup the RoamingState directory.
 :copyGraphics
     set sourceRS="%thisScriptParentDir%RoamingState"
     set destinationRS="%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState"
@@ -68,7 +69,8 @@ ECHO :: Step 2. Copy and backup the RoamingState directory.
     ECHO.
 
 :::: copy settings.json to LocalState
-ECHO :: Step 3. Copy and backup the settings.json file.
+set stepCounter=%stepCounter%+1
+ECHO :: Step %stepCounter%. Copy and backup the settings.json file.
 :copySettings
     set sourceLS="%thisScriptParentDir%LocalState\settings.json"
     set destinationLS="%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
